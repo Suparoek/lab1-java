4fbaea2f39cd16200b0e35942241862e1c3f9961
Suparoek <39317424+Suparoek@users.noreply.github.com>
2021-10-07 19:56:14 +0700

    lab1

---
+++ src/com/buzzfreeze/HelloWorld/HelloWorld.java
@@ -0,0 +1,23 @@
+/*
+ * ****************** Full program ************* 
+ *  ****************** ****************** ******************
+ *   ****************** ****************** ******************
+ * */
+package com.buzzfreeze.HelloWorld;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		//ปริ้น Hello world
+		int n=20;
+		char name ='t';
+		String name1="testing";
+		boolean mytest = true;
+		float sum = 10;
+		double sum1 = 0.10000;
+		int[] number= new int[] {1,3};
+		System.out.println("This is type Int:"+n+"\n this is type char:"+name+"\n This is type String:"+name1+
+				"\n This is type boolean:"+mytest+"\n This is type float:"+sum+"\nthis is type double:"+sum1+
+				"\n this is type array[0]"+number[0]+"\n this is type array[1]:"+number[1]);
+	}
+
+}
